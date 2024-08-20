c727b4d6b6b1ed7984d52dd81f91012500a375b1
Abhinav Karade <128953928+abhik1711@users.noreply.github.com>
2024-08-20 19:19:17 +0530

    Updated Transactions.sol

--- smartContract/contracts/Transactions.sol
+++ smartContract/contracts/Transactions.sol
@@ -1,5 +1,11 @@
 // SPDX-License-Identifier: UNLICENSED
 
+/* As a newbie in the world of Blockchain and Web3, this was a great experience to learn and know about a new language - "Solidity" 
+    The reference on how to setup and syntax accuracies was verified using the Javascript Mastery YT channel 
+    Video Link - https://youtu.be/Wn_Kb3MR_cU?si=ApqRr6nYMvBaHmCR 
+*/
+
+
 pragma solidity ^0.8.0;
 
 contract Transactions {
